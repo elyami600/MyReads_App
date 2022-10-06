8e6a17890a83956175b0cd73daeaf217472764fe
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2022-10-06 16:37:05 -0400

    Feat: new component. a serch page that allows you to find books to add to your library

---
+++ src/BookSearch.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class BookSearch extends Component {
+    render() {
+        return(
+            <div>
+                Search a Book
+
+            </div>
+        )
+    }
+}
+export default BookSearch;
