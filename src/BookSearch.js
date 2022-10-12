3e80c0768647b13b773daca247778abd5ff6c024
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2022-10-12 13:04:02 -0400

    fixed bug at the book img link

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -55,11 +55,30 @@ class BookSearch extends Component {
                                 <BookList 
                                 key={book.id}
                                 bookey={book.id}
-                                bookImageLinks={book.imageLinks.smallThumbnail}
+                                bookImageLinks={book.imageLinks}
                                 bookTitle={book.title}
                                 bookAuthors={book.authors}
 
                                 />
+                                // <li key={key}>
+                                //     <div className="book">
+                                //         <div className="book-top">
+                                //         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                                //         <div className="book-shelf-changer">
+                                //         <select >
+                                //             <option value="move" disabled>Move to...</option>
+                                //             <option value="currentlyReading">Currently Reading</option>
+                                //             <option value="wantToRead">Want to Read</option>
+                                //             <option value="read">Read</option>
+                                //             <option value="none">None</option>
+                                //         </select>
+                                //         </div>
+                                //     </div>
+                                //     <div className="book-title">{book.title}</div>
+                                //     <div className="book-authors">{book.authors}</div>
+                                // </div>
+                                    
+                                // </li>
                             ))}
                         
                         </ol>
