0930ed1e04515b620a62bfcebc680c227bb7bc97
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2022-10-13 14:21:03 -0400

    fixrd bug with image link and and detructuring props to make code readable

--- src/BookList.js
+++ src/BookList.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
+//import * as BooksAPI from './BooksAPI'
 
 class BookList extends Component {
     render() {
          //console.log("BookSearch ",this.props)
+         const { bookey, bookImageLinks, bookTitle, bookAuthors } = this.props;
         return(
             <div>
-                <li key={this.props.bookey}>
+                <li key={bookey}>
                     <div className="book">
                             <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193}} >
-                            <img src={this.props.bookImageLinks && this.props.bookImageLinks.thumbnail} alt="img" />
+                            <img src={bookImageLinks && bookImageLinks.thumbnail} alt="img" />
                             </div>
                             <div className="book-shelf-changer">
-                            <select >
+                            <select value={this.props.value}  onChange={this.props.handleUpdateShelf}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
@@ -21,8 +23,8 @@ class BookList extends Component {
                             </select>
                             </div>
                         </div>
-                        <div className="book-title">{this.props.bookTitle}</div>
-                        <div className="book-authors">{this.props.bookAuthors}</div>
+                        <div className="book-title">{bookTitle}</div>
+                        <div className="book-authors">{bookAuthors}</div>
                     </div>
                     
                 </li>
