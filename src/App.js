be3cb0641cf271d958c78061c7637e7f9119bd3d
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2022-11-09 18:35:57 -0500

    remove comment

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import MyBooks from './components/MyBooks'
 import { Route, Routes } from 'react-router-dom'
 
 
-/**
- * Convert to function component using React Hooks useState and useEffect methods instead of declaring state object and componentDidMount.
- * evise the lessons on State Management and Hooks in the classroom.
- * https://classroom.udacity.com/nanodegrees/nd0191/parts/77ac09a6-a8a2-43dd-b39f-5571db6db446/modules/c4dc8662-9370-4029-8193-ca5939577c48/lessons/f49585d0-76ce-4eb9-8218-9dbcf69dde22/concepts/176a8bb8-5da9-4afe-a64e-9e1d2f9456df
- * https://classroom.udacity.com/nanodegrees/nd0191/parts/77ac09a6-a8a2-43dd-b39f-5571db6db446/modules/c4dc8662-9370-4029-8193-ca5939577c48/lessons/b8b7fc8a-7dff-491c-8d0e-963fa289907b/concepts/26fd82cd-dd93-4ddf-8447-404713026769
- */
 const BooksApp = () => {
   const[books, setBooks] = useState([])
   const[showSearchPage, setShowSearchPage] = useState(false)
