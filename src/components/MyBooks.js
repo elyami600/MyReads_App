199b442e19e752a36bd7efe95b565cbd239f8582
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2023-02-08 13:22:25 -0500

    updated th HTML tag that is repated and use a map instead

--- src/components/MyBooks.js
+++ src/components/MyBooks.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BookList from "./BookList";
+//import BookList from "./BookList";
+import PropTypes from 'prop-types';
+import ShelfBook from "./ShelfBook";
 
 const MyBooks = ({books, handleUpdateShelf, onNavigate}) => {
-      const currentlyReading = books.filter(book => book.shelf && book.shelf === "currentlyReading");
-      const wantToRead       = books.filter(book => book.shelf && book.shelf === "wantToRead");
-      const read             = books.filter(book => book.shelf && book.shelf === "read");
+      // const currentlyReading = books.filter(book => book.shelf && book.shelf === "currentlyReading");
+      // const wantToRead       = books.filter(book => book.shelf && book.shelf === "wantToRead");
+      // const read             = books.filter(book => book.shelf && book.shelf === "read");
 
-      //const booksFiltered = props.books.filter(book => book.shelf === props.shelf);
+      //const booksFiltered = props.books.filter(book => book.shelf === props.shelf);  
+      const shelves = [
+        { id: "1", shelfName:"currentlyReading"},
+        { id: "2", shelfName:"wantToRead"},
+        { id: "3", shelfName:"read"},
+      ];
+
       
         return(
         <div>
@@ -17,57 +25,27 @@ const MyBooks = ({books, handleUpdateShelf, onNavigate}) => {
             </div>
             
             <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {currentlyReading.map((book) => currentlyReading && (
-                     <li key={book.id}>
-                     <BookList
-                     book={book}  
-                    handleUpdateShelf={(event) => handleUpdateShelf(book, event.target.value)} />
-                     </li>
-                    ))}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {wantToRead.map((book) => wantToRead && (
-                     <li key={book.id}>
-                     <BookList
-                     book={book}  
-                      handleUpdateShelf={(event) => handleUpdateShelf(book, event.target.value)} />
-                     </li>
-                    ))}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {read.map((book) =>  read && (
-                     <li key={book.id}>
-                     <BookList
-                     book={book}  
-                    handleUpdateShelf={(event) => handleUpdateShelf(book, event.target.value)} />
-                     </li>
-                    ))}
-                    </ol>
-                  </div>
-                </div>
-              </div>
+              {shelves.map((shelf) => shelf.shelfName &&(
+                <ShelfBook
+                key={shelf.id}
+                shelf={shelf}
+                books={books}
+                handleUpdateShelf={handleUpdateShelf}
+                onNavigate={onNavigate}
+                />
+              ))}
+             
             </div>
              <div className="open-search">
-              <Link to='/search' onClick={() => onNavigate}>Add a book</Link>
+              <Link to='/search' onClick={onNavigate}>Add a book</Link>
             </div>
           </div>
           
         </div>
       )
 }
+MyBooks.propTypes = {
+  books: PropTypes.array.isRequired,
+  handleUpdateShelf: PropTypes.func.isRequired,
+};
 export default MyBooks;
